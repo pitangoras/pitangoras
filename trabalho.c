79ae366c66f1ab032b90ae242f4c516dfe755d5e
pitangoras <daniel.l.rodrigues@iesb.edu.br>
2024-11-27 23:14:49 -0300

    Add files via upload

--- trabalho.c
+++ trabalho.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
     const int MAX_CONTATOS = 5;
@@ -14,27 +15,40 @@ int main() {
     int totalContatos = 0;
     int opcao;
 
-    do {
+    int numeroValido = 0;
+
+    do {       
+        system("clear");
+        
         printf("\n=== Agenda de Contatos ===\n");
         printf("1. Adicionar contato\n");
         printf("2. Listar contatos\n");
         printf("3. Sair\n");
-        printf("Escolha uma opção: ");
+        printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
         while (getchar() != '\n');
 
         switch (opcao) {
             case 1:
                 if (totalContatos < MAX_CONTATOS) {
+                    system("clear");
+                    
                     printf("\n=== Adicionar Contato ===\n");
 
                     printf("Entre com o nome: ");
                     scanf("%[^\n]", contatos[totalContatos].nome);
                     while (getchar() != '\n');
 
+                    do {
                     printf("Entre com o telefone: ");
-                    scanf("%li", &contatos[totalContatos].telefone);
-                    while (getchar() != '\n'); 
+                    numeroValido = scanf("%li", &contatos[totalContatos].telefone);
+                    if (numeroValido == 1){
+                    while (getchar() != '\n');
+                    }else if(numeroValido == 0){
+                        printf("Voce nao digitou um numero valido, tente novamente: \n");
+                        while (getchar() != '\n');
+                    }
+                           } while(numeroValido == 0);
 
                     printf("Entre com o email: ");
                     scanf("%[^\n]", contatos[totalContatos].email);
@@ -43,14 +57,19 @@ int main() {
                     totalContatos++;
                     printf("Contato adicionado com sucesso!\n");
                 } else {
+                    system("clear");
                     printf("Memoria cheia!.\n");
                 }
                 break;
 
             case 2: 
                 if (totalContatos == 0) {
+                    system("clear");
+                    
                     printf("\nNenhum contato cadastrado ainda.\n");
                 } else {
+                    system("clear");
+                    
                     printf("\n=== Lista de Contatos ===\n");
                     for (int i = 0; i < totalContatos; i++) {
                         printf("Contato %d:\n", i + 1);
@@ -60,14 +79,18 @@ int main() {
                         printf("--------------------\n");
                     }
                 }
+                printf("Pressione ENTER para continuar...");
+                getchar();
                 break;
 
             case 3:
+                system("clear");
+                
                 printf("Encerrando o programa. Até mais!\n");
                 break;
 
             default:
-                printf("Opção inválida! Por favor, tente novamente.\n");
+                printf("Opcao invalida! Por favor, tente novamente.\n");
         }
     } while (opcao != 3);
 
